b72eb59cb121dfa6b17e85c86c06f0f3f9210379
Melissa Fields <melissamfields12@gmail.com>
2022-09-20 14:55:23 -0700

    commit tree 8d0414ca1b2d130cffac6873abd87945a25df91d

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+let greeting 
+if (someValue <= 400) {
+  return 'This one is on me!'
+}
+else if (someValue > 400 && someValue <= 2000) {
+return 'That will be twenty bucks.'
+}
+else if (someValue > 2000 && someValue <= 2500) {
+  return 'I will gladly take your thirty bucks.'
+}
+else if (someValue > 2500) {
+  return "No can do."
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+ return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+    let text;
+  switch(tipAmount) {
+    case "generous":
+      return text = "Thank you so much.";
+      break;
+    case "not as generous":
+      return text = "Thank you.";
+      break;
+    default:
+      return text = "Bye.";
+  }
 }
